66370d4a615cbc625b57c0c80c9a08161cb0236f
Goukou_Ruri <zafkiel_1999@163.com>
2025-11-23 20:33:38 +0800

    Official

--- Hano.py
+++ Hano.py
@@ -1,4 +1,8 @@
 print(1)
 print('hano')
-def Hano(A,B,C)：
-    pass
+def Hano(N,Source,Target,Auxilirary):
+     if N > 0:
+          Hano(N-1,Source,Auxilirary,Target)
+          print(f"将盘子 {N} 从 {Source} 移动到 {Target}")
+          Hano(N-1,Auxilirary,Target,Source)
+Hano(3,"A","C","B")
